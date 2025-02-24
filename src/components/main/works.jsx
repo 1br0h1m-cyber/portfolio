09cfbcd1982408123c54ca91f1dbfe65f71647ec
1br0h1m-cyber <normahmatovibrohim0409@gmail.com>
2025-02-24 16:11:04 +0500

    works page

--- src/components/main/works.jsx
+++ src/components/main/works.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import onlineShop from '../../assets/online-shop.png'
 import webSchool from '../../assets/web-school.png'
+import Weather from '../../assets/weather.png'
 
 const Works = () => {
   return (
@@ -27,6 +28,17 @@ const Works = () => {
           <h1 className='px-2 py-1 font-bold text-[20px]'>Stack:</h1>
           <h3 className='px-2 py-1'>React, Tailwind CSS, © 3rd School</h3>
           <a href="https://school-ibrohim.vercel.app/" className='flex justify-center items-center h-10 w-20 font-medium rounded-lg shadow-lg text-white bg-blue-600 hover:bg-blue-700 transition-all mx-2 mt-5'>Visit</a>
+        </div>
+      </div>
+
+      <div className='flex flex-col justify-center items-center shadow-lg rounded-lg w-[90%] bg-gray-50 pb-8 m-5 md:mt-7 lg:mt-10'>
+        <img src={Weather} alt="" className='p-4 sm:p-6 md:p-8 lg:p-12 pt-8 pb-4 md:pb-8'/>
+        <div className='w-[90%]'>
+          <h1 className='px-2 py-1 font-bold text-[20px]'>Weather website</h1>
+          <p  className='px-2 py-1 text-[15px] md:text-[18px]'>A weather website provides real-time updates on temperature, humidity, and precipitation for different locations. Users can check forecasts for the next few days, helping them plan their activities accordingly.</p>
+          <h1 className='px-2 py-1 font-bold text-[20px]'>Stack:</h1>
+          <h3 className='px-2 py-1'>Html,Css,Js; © OpenWeather</h3>
+          <a href="https://1br0h1m-cyber.github.io/weather/" className='flex justify-center items-center h-10 w-20 font-medium rounded-lg shadow-lg text-white bg-blue-600 hover:bg-blue-700 transition-all mx-2 mt-5'>Visit</a>
         </div>
       </div>
 
